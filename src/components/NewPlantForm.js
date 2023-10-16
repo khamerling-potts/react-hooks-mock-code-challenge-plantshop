084c0b4b73b3814031ec983ab91515f96fbac586
Kyra Hamerling-Potts <khamerling-potts@wustl.edu>
2023-10-16 14:06:24 -0400

    plant challenge done without advanced deliverables

--- src/components/NewPlantForm.js
+++ src/components/NewPlantForm.js
@@ -1,13 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 
-function NewPlantForm() {
+function NewPlantForm({ onAddPlant }) {
+  const [newPlant, setNewPlant] = useState({
+    name: "",
+    image: "",
+    price: "",
+  });
+
+  function handleFormSubmit(e) {
+    e.preventDefault();
+    const configObj = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newPlant),
+    };
+    fetch(`http://localhost:6001/plants`, configObj)
+      .then((res) => res.json())
+      .then((plant) => onAddPlant(plant));
+  }
+
+  function handleInputChange(e) {
+    const value = e.target.value;
+    const key = e.target.name;
+    setNewPlant({ ...newPlant, [key]: value });
+  }
+
   return (
     <div className="new-plant-form">
       <h2>New Plant</h2>
-      <form>
-        <input type="text" name="name" placeholder="Plant name" />
-        <input type="text" name="image" placeholder="Image URL" />
-        <input type="number" name="price" step="0.01" placeholder="Price" />
+      <form onSubmit={handleFormSubmit}>
+        <input
+          type="text"
+          name="name"
+          placeholder="Plant name"
+          value={newPlant.name}
+          onChange={handleInputChange}
+        />
+        <input
+          type="text"
+          name="image"
+          placeholder="Image URL"
+          value={newPlant.image}
+          onChange={handleInputChange}
+        />
+        <input
+          type="text"
+          name="price"
+          step="0.01"
+          placeholder="Price"
+          value={newPlant.price}
+          onChange={handleInputChange}
+        />
         <button type="submit">Add Plant</button>
       </form>
     </div>
